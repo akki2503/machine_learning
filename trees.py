b83931b54d5db41b7f090d463e03666a370b6e64
akki2503 <37500117+akki2503@users.noreply.github.com>
2018-06-26 17:38:12 +0530

    Update trees.py

--- trees.py
+++ trees.py
@@ -1,4 +1,5 @@
 #function to calculate Shannon entropy of a dataset
+import operator
 
 from math import log
 def calcShannonEnt(dataSet):
@@ -29,6 +30,49 @@ def createDataSet():
 #function to split dataset
 
 def splitDataSet(dataSet, axis, value):
+  retDataSet = []
+  for featVec in dataSet:
+     if featVec[axis] == value:
+        reducedFeatVec = featVec[:axis]
+        reducedFeatVec.extend(featVec[axis+1:])
+        retDataSet.append(reducedFeatVec)
+  return retDataSet
+
+#choosing best feature to split on
+
+def chooseBest(dataSet):
+  numfeatures = len(dataSet[0]) - 1
+  baseEntropy = calcShannonEnt(dataSet)
+  bestInfogain = 0.0
+  bestFeature = -1
+  for i in range(numFeatures):
+    featList = [example[i] for example in dataSet]
+    uniqueVals = set(featList)
+    newEntropy = 0.0
+    for value in uniqueVals:
+      subDataSet = splitDataSet(dataSet, i, value)
+      prob = len(subDataSet)/float(len(dataSet))
+      newEntropy += prob*clacShannonEnt(subDataSet) 
+    infoGain = baseEntropy - newEntropy
+    if (infoGain > bestInfoGain)
+      bestInfogain = infoGain
+      bestFeature = i
+  return bestFeature
+  
+  def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+      if vote not in classCount.keys(): classCount[vote] = 0
+      classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1), reverse = True)
+    return sortedClassCount[0][0]
+  
+def createTree(dataSet, labels):
+  classList = [example[-1] for example in dataSet]
+  
+
+
+        
   
 
   
