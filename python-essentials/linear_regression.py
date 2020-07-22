7aa8cb5356bf2b6b5fb32431dffe3272fe911c46
Akshat Mandloi <akshat.2597@gmail.com>
2020-07-23 02:36:03 +0530

    Kaggle Titnanic Simple Logistic Regression

---
+++ python-essentials/linear_regression.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import seaborn as sns
+sns.set()
+
+data = pd.read_csv('/home/akki/Documents/python_projects/novartis_data_science_challenge/6a62a5b4967411ea/Dataset/Train.csv')
+data
